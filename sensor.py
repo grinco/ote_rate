bbfd12a5174d40e2198246c31e47623151620a10
Michal Base <mikeb@seznam.cz>
2023-01-15 14:53:42 +0000

    get next day prices

--- sensor.py
+++ sensor.py
@@ -1,41 +1,37 @@
-"""Platform for sensor integration."""
-from homeassistant.helpers.entity import Entity
 from homeassistant.components.sensor import (
-    DEVICE_CLASS_MONETARY,
     SensorEntity,
-    SensorEntityDescription,
 )
-
-""" External Imports """
 import requests
-import json
-import datetime
+from datetime import datetime, timedelta
 import logging
-
-
-""" Constants """
-NATIVE_UNIT_OF_MEASUREMENT = "EUR/MWh"
-DEVICE_CLASS = "monetary"
+from .const import (
+    NEXT_DAY_AVAILABLE_ATTRIBUTE,
+    NEXT_DAY_PREFIX,
+    DEVICE_CLASS,
+    NATIVE_UNIT_OF_MEASUREMENT,
+)
 
 _LOGGER = logging.getLogger(__name__)
 
-def setup_platform(hass, config, add_entities, discovery_info=None):
+
+def setup_platform(hass, config, add_entities, discovery_info=None) -> None:
     """Set up the sensor platform."""
-    add_entities([OTERateSensor()], update_before_add=True)
+    add_entities([OTERateSensor(hass)], update_before_add=True)
 
 
 class OTERateSensor(SensorEntity):
     """Representation of a Sensor."""
 
-    def __init__(self):
+    def __init__(self, hass) -> None:
         """Initialize the sensor."""
         self._value = None
         self._attr = None
+        self._hass = hass
 
     @property
     def name(self):
         """Return the name of the sensor."""
-        return 'Current OTE Energy Cost'
+        return "Current OTE Energy Cost"
 
     @property
     def native_value(self):
@@ -62,53 +58,78 @@ class OTERateSensor(SensorEntity):
         """Return True if entity is available."""
         return self._available
 
-    def update(self):
+    def update(self) -> None:
         """Fetch new state data for the sensor.
 
         This is the only method that should fetch new data for Home Assistant.
         """
         self._get_current_value()
 
-
-    def _get_current_value(self):
-        """ Parse the data and return value in EUR/kWh
-        """
+    def _get_current_value(self) -> None:
+        """Parse the data and return value in EUR/kWh"""
 
         try:
-          current_cost = 0
-          cost_history = dict()
-          history_index = 0
-          cost_string = "Cena (EUR/MWh)"
-          hour_string = "Hodina"
-          cost_data = "https://www.ote-cr.cz/cs/kratkodobe-trhy/elektrina/denni-trh/@@chart-data"
 
-          date = datetime.datetime.now()
-          params = dict (
-              date = date.strftime('%Y-%m-%d')
-          )
+            now = datetime.now()
+            next_day = datetime.now() + timedelta(days=1)
 
-          response = requests.get(url=cost_data, params=params)
-          json = response.json()
-          cost_axis = ""
-          hour_axis = ""
-          for key in json['axis'].keys():
-              if json['axis'][key]['legend'] == cost_string:
-                  cost_axis = key
-              if json['axis'][key]['legend'] == hour_string:
-                  hour_axis = key
+            today_costs: DateCosts = self._get_costs_for_date(now)
+            next_day_costs: DateCosts = self._get_costs_for_date(next_day)
 
+            attributes = dict()
 
-          for values in json['data']['dataLine']:
-              if values['title'] == cost_string:
-                  for data in values['point']:
-                     history_index = int(data[hour_axis])-1
-                     cost_history[history_index] = float(data[cost_axis])
-                  current_cost = cost_history[date.hour]
+            for hour, cost in today_costs.cost_history.items():
+                attributes[hour] = cost
 
+            for hour, cost in next_day_costs.cost_history.items():
+                attributes[f"{NEXT_DAY_PREFIX}{hour}"] = cost
 
-          self._value = current_cost
-          self._attr = cost_history
-          self._available = True
+            attributes[NEXT_DAY_AVAILABLE_ATTRIBUTE] = (
+                len(next_day_costs.cost_history) > 0
+            )
+
+            self._value = today_costs.date_cost
+            self._attr = attributes
+            self._available = True
         except:
-          self._available = False
-          _LOGGER.exception("Error occured while retrieving data from ote-cr.cz.")
+            self._available = False
+            _LOGGER.exception("Error occured while retrieving data from ote-cr.cz")
+
+    def _get_costs_for_date(self, date: datetime) -> "DateCosts":
+        current_cost = 0
+        cost_history = dict()
+        history_index = 0
+        cost_string = "Cena (EUR/MWh)"
+        hour_string = "Hodina"
+        cost_data = (
+            "https://www.ote-cr.cz/cs/kratkodobe-trhy/elektrina/denni-trh/@@chart-data"
+        )
+
+        params = dict(date=date.strftime("%Y-%m-%d"))
+
+        response = requests.get(url=cost_data, params=params, timeout=60)
+
+        json = response.json()
+
+        cost_axis = ""
+        hour_axis = ""
+        for key in json["axis"].keys():
+            if json["axis"][key]["legend"] == cost_string:
+                cost_axis = key
+            if json["axis"][key]["legend"] == hour_string:
+                hour_axis = key
+
+        for values in json["data"]["dataLine"]:
+            if values["title"] == cost_string:
+                for data in values["point"]:
+                    history_index = int(data[hour_axis]) - 1
+                    cost_history[history_index] = float(data[cost_axis])
+                current_cost = cost_history[date.hour]
+
+        return DateCosts(cost_history, current_cost)
+
+
+class DateCosts:
+    def __init__(self, cost_history: dict, date_cost: float) -> None:
+        self.cost_history = cost_history
+        self.date_cost = date_cost
